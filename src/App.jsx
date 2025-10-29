f7e79bca4721ce1a1ce01f0ab5812d147c397504
shanejeremich <shane.jeremich@gmail.com>
2025-10-29 15:49:22 -0600

    style(App): update structure and import styles
    - Changed fragment to a div in App component for better structure
    - Updated import statement to include main.css in App
    - Removed index.css import from main.jsx and replaced with globals.css
    - Added globals.css with comprehensive styles for the application
    
    This improves the overall styling consistency and structure of the application.

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import Footer from "@design-system/layout/Footer";
 import Navbar from "@design-system/navigation/Navbar";
 import HomePage from "@design-system/pages/HomePage";
 
+import "@assets/styles/main.css";
+
 function App() {
   return (
-    <>
+    <div>
       <Navbar />
       <HomePage />
       <Footer />
-    </>
+    </div>
   );
 }
 
